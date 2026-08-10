2f3e63f75910f98118f07d5cf46940fd341e1b79
Shanmukh <i.v.shanmukh@gmail.com>
2026-08-10 11:14:51 +0530

    added new line

--- GitApp/src/main/java/com/shanInfotech/gitJavaApp/GitApp/App.java
+++ GitApp/src/main/java/com/shanInfotech/gitJavaApp/GitApp/App.java
@@ -1,8 +1,6 @@
 package com.shanInfotech.gitJavaApp.GitApp;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
